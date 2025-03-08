8ee4f7c77f74b1ee033e5cffbb07fb72fff97a0f
Nurbu Lama <lamanurbu2@gmail.com>
2025-03-07 19:23:00 -0500

    First comit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+const App = () => {
+  return(
+    <div className='Main'>
+      <div className='mbody'>
+        <h1>Bio Exam Test</h1>
+        <h3>Are you ready for the bio test?</h3>
+        <h5># of Questions</h5>
+        <div className='card'>
+          <h3>asdasa</h3>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
