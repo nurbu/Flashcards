4b0d321aaa839d35725fee0a90e09837c7fc651d
Your Name <you@example.com>
2025-03-10 02:49:41 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Cardtext from './component/Card'
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [back, setBack] = useState(true)
+
+  const add = () => setCount(prevCount => (prevCount+1===10 ? 0: prevCount+1));
+  const sub = () => setCount(prevCount => (prevCount-1===-1 ? 9: prevCount-1));
+
   return(
     <div className='Main'>
       <div className='mbody'>
         <h1>Bio Exam Test</h1>
         <h3>Are you ready for the bio test?</h3>
-        <h5># of Questions</h5>
-        <div className='card'>
-          <h3>asdasa</h3>
+        <h5>10 Questions</h5>
+        <div className='Card' onClick={() => setBack(!back)} >
+          <Cardtext qnum={count} qora={back} />
         </div>
+        <div>
+        <button onClick={sub}>⬅</button>
+        <button onClick={add}>➡</button>
       </div>
+      </div>
+      
     </div>
   )
 }
